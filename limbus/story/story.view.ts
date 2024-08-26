b779705ecc21810271f828ba8801613d54929660
kimght <bamboo-hatted-kim@proton.me>
2024-08-26 16:56:53 +0300

    fix: use latest chapters gist

--- limbus/story/story.view.ts
+++ limbus/story/story.view.ts
@@ -67,7 +67,7 @@ namespace $.$$ {
 		}
 		
 		chapters_url() {
-			return "https://gist.githubusercontent.com/kimght/7049a925e4d0c33517cde112103758f8/raw/2c9249622d6025e73a97b1f612b3f80f98368576/chapters_list.json"
+			return "https://gist.githubusercontent.com/kimght/7049a925e4d0c33517cde112103758f8/raw/chapters_list.json"
 		}
 		
 		@ $mol_mem
